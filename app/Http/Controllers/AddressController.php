8439b8aadac81bd126d6526c859351d6848f142b
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-11-16 21:56:04 +0700

    add api address: delete

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -80,4 +80,16 @@ class AddressController extends Controller
 
         return new AddressResource($address);
     }
+
+    public function delete(int $idContact, int $idAddress): JsonResponse
+    {
+        $user = Auth::user();
+        $contact = $this->getContact($user, $idContact);
+        $address = $this->getAddress($contact, $idAddress);
+        $address->delete();
+
+        return response()->json([
+            'data' => 'Delete success'
+        ])->setStatusCode(200);
+    }
 }
