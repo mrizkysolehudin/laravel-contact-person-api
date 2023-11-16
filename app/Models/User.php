ca9d85f2e6e5b4d6782545fdd0a9ab08d32cb853
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-11-16 18:42:09 +0700

    add api: register, login & add middleware: ApiAuthMiddleware

---
+++ app/Models/User.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Model implements Authenticatable
+{
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $keyType = 'int';
+    public $timestamps = true;
+    public $incrementing = true;
+
+    protected $fillable = [
+        'name',
+        'username',
+        'password'
+    ];
+
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class, 'user_id', 'id');
+    }
+
+    public function getAuthIdentifierName()
+    {
+        return 'username';
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->username;
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'token';
+    }
+
+    public function getRememberToken()
+    {
+        return $this->token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->token = $value;
+    }
+
+
+}
+
